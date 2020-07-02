d6dc8383e245192d03c49f6a074876dfbae9cef2
Bennett, Ryan <ryanjbennett@live.com>
2020-07-01 21:46:14 -0500

    init - migrations, initialized, service and repositories folders added.

---
+++ TheGathering/TheGathering.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheGathering.Web.MvcApplication" Language="C#" %>
---
+++ TheGathering/TheGathering.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheGathering.Web.Startup))]
+namespace TheGathering.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
