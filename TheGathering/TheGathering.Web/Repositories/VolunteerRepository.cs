892ee2b4ce3c306e2bcf6e25279ea811f3b663f1
Evan Bagwell <etb@wi.rr.com>
2020-07-07 14:56:26 -0500

    Framework Volunteer

---
+++ TheGathering/TheGathering.Web/Repositories/VolunteerRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheGathering.Web.Repositories
+{
+    public class VolunteerRepository
+    {
+    }
+}
---
+++ TheGathering/TheGathering.Web/Service/VolunteerService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheGathering.Web.Service
+{
+    public class VolunteerService
+    {
+    }
+}
