22ac2620af46ff635ab8ef2364ae08bff86001a0
Adam Miller <theadammiller56@gmail.com>
2020-07-07 14:35:30 -0500

    Added Model pre- (successful) Migration

---
+++ TheGathering/TheGathering.Web/Models/VolunteerEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheGathering.Web.Models
+{
+    public class VolunteerEvent
+    {
+        public int Id { get; set; }
+        public DateTime StartingShiftTime { get; set; }
+        public TimeSpan ShiftSpan { get; set; }
+        public int OpenSlots { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+    }
+}
