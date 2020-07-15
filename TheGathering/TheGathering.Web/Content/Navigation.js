ae56823a6e486e074dcfed7ff1ad76ec085dbde4
Evan Bagwell <etb@wi.rr.com>
2020-07-15 10:38:20 -0500

    Navigation Bar

---
+++ TheGathering/TheGathering.Web/Content/Navigation.js
@@ -0,0 +1,40 @@
+﻿const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+
+        //Toggle Nav
+        nav.classList.toggle(`nav-active`);
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+     
+            if (link.style.animation) {
+                link.style.animation = ``;
+            } else {
+                link.style.animation = `navLinkFade 0s ease forwards`;
+            }
+
+        });
+        //burger Animation
+        burger.classList.toggle('toggle');
+    });
+}  
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
+
+navSlide();
