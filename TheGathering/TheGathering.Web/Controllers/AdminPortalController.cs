e2cad58cf52929f43d29cac589c992f43a0e51f1
Jack Rosenbecker <link2003.2013@gmail.com>
2020-07-16 14:58:54 -0500

    Basic pages for Admin control

---
+++ TheGathering/TheGathering.Web/Controllers/AdminPortalController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TheGathering.Web.Models;
+using TheGathering.Web.Service;
+using TheGathering.Web.Services;
+
+namespace TheGathering.Web.Controllers
+{
+    public class AdminPortalController : Controller
+    {
+        private VolunteerService volunteerService = new VolunteerService();
+        private MealSiteService mealService = new MealSiteService();
+
+        // GET: AdminPortal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            return View();
+        }
+
+        public ActionResult MealSites()
+        {
+            return View(mealService.GetAllMealSites());
+        }
+
+        public ActionResult MealSiteDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MealSite mealSite = mealService.GetMealSiteById((int)id);
+            if (mealSite == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mealSite);
+        }
+    }
+}
