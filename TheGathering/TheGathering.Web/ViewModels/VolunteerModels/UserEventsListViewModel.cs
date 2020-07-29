b554c36f866caf6bdfed6fe45ddee25ea3a7e07c
Rebecca Gilpatric <rebeccagilpatric@gmail.com>
2020-07-29 12:41:41 -0500

    Not working-- separate events by time

--- TheGathering/TheGathering.Web/ViewModels/VolunteerModels/UserEventsListViewModel.cs
+++ TheGathering/TheGathering.Web/ViewModels/VolunteerModels/UserEventsListViewModel.cs
@@ -10,4 +10,17 @@ namespace TheGathering.Web.ViewModels.VolunteerModels
         public List<VolunteerEvent> VolunteerEvents { get; set; }
         public List<VolunteerEvent> CancelledEvents { get; set; }
     }
+
+    public SeparateCurrentAndOldEvents()
+    {
+        List<VolunteerEvent> PastEvents = new List<VolunteerEvent>;
+        List<VolunteerEvent> CurrentEvents = new List<VolunteerEvent>;
+        foreach(var item in VolunteerEvents)
+        {
+            if (item.StartingShiftTime > DateTime.Now)
+                CurrentEvents.Add(item);
+            else
+                PastEvents.Add(item);
+        }
+    }
 }
