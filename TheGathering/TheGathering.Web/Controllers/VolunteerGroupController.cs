35a083ec7ac09ca898a07eacac8322e079de45cb
Vaishnavi Manikonda <vaishnavi.manikonda@outlook.com>
2020-07-30 14:21:54 -0500

    Can only sign up more than 1 volunteer in Group sign up

--- TheGathering/TheGathering.Web/Controllers/VolunteerGroupController.cs
+++ TheGathering/TheGathering.Web/Controllers/VolunteerGroupController.cs
@@ -51,13 +51,25 @@ namespace TheGathering.Web.Controllers
             int numVolunteers = signUpGroupViewModel.VolunteerSlots;
             int eventId = signUpGroupViewModel.VolunteerEventID;
             int volunteerId = signUpGroupViewModel.VolunteerGroupLeaderID;
+            signUpGroupViewModel.VolunteerEvent = _eventService.GetEventById(eventId);
+            signUpGroupViewModel.VolunteerGroupLeader = _service.GetLeaderById(volunteerId);
             var origOpenSlots = _eventService.GetEventById(eventId).OpenSlots;
             var volunteerEvent = _eventService.GetEventById(eventId);
-            if (origOpenSlots < numVolunteers)
-                return RedirectToAction("Index"); //TODO- Eventually make a view to redirect to
-            _service.ReduceOpenSlots(volunteerEvent, origOpenSlots, numVolunteers);
-            _service.AddVolunteerGroupVolunteerEvent(volunteerId, eventId, numVolunteers);
-            return RedirectToAction("GroupEventsList");
+            if (signUpGroupViewModel.VolunteerSlots<1)
+            {
+                ModelState.AddModelError("VolunteerSlots", "The number of volunteers cannot be less than 1");
+            }
+           
+            if (ModelState.IsValid)
+            {
+                if (origOpenSlots < numVolunteers)
+                    return RedirectToAction("Index"); //TODO- Eventually make a view to redirect to
+                _service.ReduceOpenSlots(volunteerEvent, origOpenSlots, numVolunteers);
+                _service.AddVolunteerGroupVolunteerEvent(volunteerId, eventId, numVolunteers);
+                return RedirectToAction("GroupEventsList");
+            }
+
+            return View(signUpGroupViewModel);
         }
         public ActionResult EventAlreadyRegistered()
         {
