8fc36577f2a5d95b17d5bbf867e36f0667400a09
Vaishnavi Manikonda <vaishnavi.manikonda@outlook.com>
2020-07-30 09:32:37 -0500

    Separated conjoined words on all VolunteerGroup Pages

--- TheGathering/TheGathering.Web/Models/VolunteerGroupLeader.cs
+++ TheGathering/TheGathering.Web/Models/VolunteerGroupLeader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,15 +9,23 @@ namespace TheGathering.Web.Models
     public class VolunteerGroupLeader
     {
         public int Id { get; set; }
+        [Display(Name = "Leader First Name")]
         public string LeaderFirstName { get; set; }
+        [Display(Name = "Leader Last Name")]
         public string LeaderLastName { get; set; }
+        [Display(Name = "Leader Birthday")]
         public DateTime LeaderBirthday { get; set; }
+        [Display(Name = "Leader Email")]
         public string LeaderEmail { get; set; }
+        [Display(Name = "Leader Phone Number")]
         public string LeaderPhoneNumber { get; set; }
+        [Display(Name = "Group Name")]
         public string GroupName { get; set; }
+        [Display(Name = "Newsletter")]
         public Boolean SignUpForNewsLetter { get; set; }
         public List<VolunteerGroupVolunteerEvent> VolunteerGroupVolunteerEvents { get; set; } = new List<VolunteerGroupVolunteerEvent>();
         public String ApplicationUserId { get; set; }
+        [Display(Name = "Total Group Members")]
         public int TotalGroupMembers { get; set; }
         
         
