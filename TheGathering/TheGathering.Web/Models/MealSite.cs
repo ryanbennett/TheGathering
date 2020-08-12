d1e90e0eec404d62e8c4d2da9958838d4daf4d63
Jack Rosenbecker <link2003.2013@gmail.com>
2020-08-12 10:23:13 -0500

    Fix time conversion function

--- TheGathering/TheGathering.Web/Models/MealSite.cs
+++ TheGathering/TheGathering.Web/Models/MealSite.cs
@@ -135,13 +135,14 @@ namespace TheGathering.Web.Models
 
         }
 
-        public static string twelveTo24HourTime(string twelveHourTime)
+        public static string ToTweleveHourTime(string twelveHourTime)
         {
             try
             {
                 string[] split = twelveHourTime.Split(':');
                 int hr = int.Parse(split[0]);
-                int min = int.Parse(split[1]);
+                int minInt = int.Parse(split[1]); //Use parse to verify that the minute is valid
+                string min = split[1];
                 int newHr = hr % 12;
                 if (newHr == 0)
                     newHr = 12;
