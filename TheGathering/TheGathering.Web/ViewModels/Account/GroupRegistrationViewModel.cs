8056cb8df40d9c09b5367ccc8dd4a6f83959694d
Adam Miller <theadammiller56@gmail.com>
2020-08-13 10:13:06 -0500

    Adding display Names for Registration View Models.

--- TheGathering/TheGathering.Web/ViewModels/Account/GroupRegistrationViewModel.cs
+++ TheGathering/TheGathering.Web/ViewModels/Account/GroupRegistrationViewModel.cs
@@ -10,16 +10,22 @@ namespace TheGathering.Web.ViewModels.Account
     public class GroupRegistrationViewModel : RegisterViewModel
     {
         [Required] //This makes the FirstName field required
+        [Display(Name = "Leader First Name")]
         public string LeaderFirstName { get; set; }
         [Required]
+        [Display(Name = "Leader Last Name")]
         public string LeaderLastName { get; set; }
         [Required]
         public string Email { get; set; }
+        [Display(Name = "Leader D.O.B.")]
         public DateTime LeaderBirthday { get; set; }
+        [Display(Name = "Group Name")]
         public string GroupName { get; set; }
+        [Display(Name = "Leader Phone Number")]
         public string LeaderPhoneNumber { get; set; }
         public bool SignUpForNewsLetter { get; set; }
         public string ApplicationUserId { get; set; }
+        [Display(Name = "Total Group Members")]
         public int TotalGroupMembers { get; set; }
     }
 }
